561f95ae2fdf8d7c9aaa233a5f999d4fb6dc29fe
Mu Lu <murphytalk@gmail.com>
2016-04-14 09:06:17 +0900

    leet code #151 - c solution

--- leecode/src/leecode.cpp
+++ leecode/src/leecode.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include <string>
 #include <vector>
+#include <cstdlib>
 
 using namespace std;
 class No_151 : public ::testing::Test{
@@ -19,7 +20,6 @@ public:
 
 		const int size = ++end -start + 1;
 		char* buf = new char [size];
-		memset(buf, 0, size);
 		char* pb = buf;
 		const char *p1 = const_cast<const char*>(start);
 		char *p2 = end;
@@ -52,6 +52,55 @@ public:
 		s.replace(s.begin(), s.end(), buf, buf + actual_size);
 		delete []buf;
 	}
+
+
+	void reverseWords2(char *s) {
+		int len = strlen(s);
+		char *start = s;
+		char *end = start + len - 1;
+
+		for (; *start == ' '; ++start);
+		for (; *end == ' '; --end);
+
+		if (start > end) {
+			s[0] = 0;
+			return;
+		}
+
+		int size = ++end - start + 1;
+		char* buf = (char*) malloc(size);
+		char* pb = buf;
+		char *p1 = start;
+		char *p2 = end;
+		char *p = p2;
+		int n;
+		int actual_size = 0;
+
+		while (p >= p1) {
+			--p;
+			if (*p == ' ') {
+				//found a word
+				n = p2 - (p + 1);
+				strncpy(pb, p + 1, n);
+				pb += n;
+				*pb++ = ' ';
+				actual_size += n + 1;
+				//if there are more spaces, skip them
+				for (; *(p - 1) == ' '&& p > p1; --p);
+				p2 = p;  //both pointed to space
+			}
+		}
+		if (p2 != p) {
+			n = p2 - (p + 1);
+			strncpy(pb, p + 1, n);
+			pb += n;
+			*pb++ = ' ';
+			actual_size += n + 1;
+		}
+		strncpy(s, buf, --actual_size);
+		s[actual_size] = 0;
+		free(buf);
+	}
 };
 
 TEST_F(No_151,the_sky_is_blue){
@@ -78,6 +127,14 @@ TEST_F(No_151, one) {
 	EXPECT_EQ(s1, s2);
 }
 
+TEST_F(No_151, space_one) {
+	std::string s1 = " 1";
+	const std::string s2 = "1";
+
+	reverseWords(s1);
+	EXPECT_EQ(s1, s2);
+}
+
 TEST_F(No_151, a) {
 	std::string s1 = "a";
 	const std::string s2 = "a";
@@ -91,5 +148,22 @@ TEST_F(No_151, lots_of_space_in_the_middle) {
 	const std::string s2 = "b a";
 
 	reverseWords(s1);
+	EXPECT_EQ(s1, s2);
+}
+
+//c version
+TEST_F(No_151, c_the_sky_is_blue) {
+	char s1 [] = "the sky is blue";
+	const std::string s2 = "blue is sky the";
+
+	reverseWords2(s1);
+	EXPECT_EQ(s1, s2);
+}
+
+TEST_F(No_151, c_space_one) {
+	char s1 [] = " 1";
+	const std::string s2 = "1";
+
+	reverseWords2(s1);
 	EXPECT_EQ(s1, s2);
 }
