418215220f338a0f6ae261242b918fb91743cd7a
Mu Lu <murphytalk@gmail.com>
2016-09-27 17:38:37 +0900

    revised move exmaple

--- src/cpp_notes/c++11/move.cpp
+++ src/cpp_notes/c++11/move.cpp
@@ -1,49 +1,89 @@
 #include "catch.hpp"
 #include <iostream>
+#include <string>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
 namespace Cxx11Test{
     class Rvalue{
-    public: 
-        int *p;
-        Rvalue(){
-           p = new int;
-           std::cout<<"created p "<<p<<std::endl;
+    public:
+		int id;
+		string s;
+        Rvalue(int i,const char* p):id(i),s(p){
+           std::cout<<"created id="<<id<<"(" <<s<<")" << std::endl;
         }
         ~Rvalue(){
-           std::cout<<"deleted p "<<p<<std::endl;
-           delete p;
+           std::cout<<"deleted id=" << id <<"("<<s<< ")" << std::endl;
         }
+		Rvalue(const Rvalue& r) = delete;
         Rvalue(Rvalue&& r){
-           std::cout<<"move constructor,move resource p "<<r.p<<std::endl; 
-           p = r.p;
-           r.p = nullptr;
+           std::cout<<"move constructor,move resource ( "<<r.s<< ") from id="<<r.id<<" to id="<<id << std::endl;
+		   id = r.id;
+		   s = std::move(r.s);
         }
-        Rvalue& operator=(Rvalue&& r){
-            std::cout<<"move assigment, move resouce p"<<r.p<<std::endl;
+		/* without move assigment operator, the following code won't compile
+			Rvalue r(100,"Destination");
+			Rvalue r = make_rvalue(1,"Source");
+		*/
+		Rvalue& operator=(Rvalue&& r){
+			std::cout << "move assignment,move resource ( " << r.s << ") from id=" << r.id << " to id=" << id << std::endl;
             if(this!=&r){
-                std::cout<<"deleted existing p "<<p<<std::endl;
-                delete p;
-                p = r.p;
-                r.p = nullptr;
+				s = std::move(r.s);
             }
+			return *this;
         }
     };
     
-    Rvalue make_rvalue(){
+    Rvalue make_rvalue(int id,const char*s){
         std::cout<<"make rvalue:";
-        return Rvalue();
+        return Rvalue(id,s);
     }
     
     void accept_rvalue(Rvalue&& r){
-        std::cout<<"accepted p "<<r.p<<std::endl;
+        std::cout<<"accepted id="<<r.id<<"("<<r.s << ")" <<std::endl;
+		cout << "end of accept_rvalue()"<<endl;
     }
-    
-    TEST_CASE("C++ 11 move assigment","[c++11]"){
+
+	TEST_CASE("C++ 11 move assigment 1", "[c++11]") {
+		Rvalue r(200, "Orginal");
+		auto& v = r;
+		cout << "orginal addr " << &r << endl;
+		cout << "dest addr " << &v << endl;
+	}
+
+
+    TEST_CASE("C++ 11 move assigment 2","[c++11]"){
         std::cout<<"The destination:";
-        Rvalue r;
-        r = make_rvalue();
+        Rvalue r(100,"Destination");
+		r = make_rvalue(1,"Source");
     }
     
-    TEST_CASE("C++ 11 move constructor","[c++11]"){
-        accept_rvalue(Rvalue());
+    TEST_CASE("C++ 11 rvalue reference","[c++11]"){
+		cout << "START" << endl;
+		//the following line won't compile if accept_rvalue is defined as accept_rvalue(Rvalue& r)
+        accept_rvalue(Rvalue(1000,"Temp"));
+		cout << "end of calling accept_rvalue()" << endl;
+		cout << "END" << endl;
     }
+
+
+	TEST_CASE("C++ 11 move constructor 1", "[c++11]") {
+		vector<Rvalue> v;
+		//temp object will trigger rvalue move constructor
+		v.push_back(Rvalue(1, "data1"));
+		v.push_back(Rvalue(2, "data2"));
+	}
+
+	TEST_CASE("C++ 11 move constructor 2", "[c++11]") {
+		vector<Rvalue> v;
+
+		Rvalue r1(10, "data10");
+		Rvalue r2(20, "data20");
+
+		//won't compile if don't use std::move due to miss Rvalue(const RValue&) => needed by vector
+		v.push_back(std::move(r1));
+		v.push_back(std::move(r2));
+	}
 }
