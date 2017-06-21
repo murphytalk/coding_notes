04fb04ee7f870da020ac016ac69aaeaa9df4139b
murphytalk <murphytalk@gmail.com>
2017-06-21 11:37:31 +0000

    trick: get private member

--- src/cpp_notes/stl/tricks.cpp
+++ src/cpp_notes/stl/tricks.cpp
@@ -1,6 +1,7 @@
 #include "catch.hpp"
 #include <vector>
 #include <algorithm>
+#include <string>
 
 using namespace std;
 
@@ -26,7 +27,35 @@ namespace CxxSTL{
         }
     }
 
-	/* This is dirty but demostrates C++ data model
+	/* This is dirty but demostrates C++ data model.	
+	 * Question : given a Data type object instance, how to get its _private_data ?
 	*/
+	class Data {
+	private:
+		int _private_data;
+	public:
+		int public_data;
+		std::string str;
+		Data(int data) {
+			_private_data = data;
+		}
+	};
+	// The solution : construct a class with same data members in same order, but can be visit via public member function
+	class Data2 {
+	private:
+		int _private_data;
+	public:
+		int public_data;
+		std::string str;
+		int get_private_data() { return _private_data; }
+	};
 	
+	TEST_CASE("Get private data member", "[trick]") {
+		const int v = 1234;
+		Data d(v);
+		Data2 *p = reinterpret_cast<Data2*>(&d);
+		REQUIRE(p->get_private_data() == v);
+	}
+
+
 }
