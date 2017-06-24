896c2e2fbcc4e6aec8dbd78081b688e8aa4791bf
murphytalk <murphytalk@gmail.com>
2017-06-24 14:09:58 +0000

    Max value in contiguous subarray

--- src/hackerrank/hackerrank.cpp
+++ src/hackerrank/hackerrank.cpp
@@ -5,6 +5,8 @@
 #include <algorithm>
 #include <set>
 #include <sstream>
+#include <functional>
+#include <deque>
 #include "src/utils/utils.h"
 
 using namespace std;
@@ -111,27 +113,16 @@ public:
 	}
 };
 
-static std::pair<string,string> lru_cache_test(const char* iff, const char* off){
+/*
+Hacker ranker gives test data in STDIN and checks result in STDOUT.
+This helper feeds the input/output downloaded from hacker ranker to a user defined function.
+It returs a pair where expected output is the first and our output is the second.
+*/
+static pair<string, string> hacker_ranker_tester(const char* iff, const char* off, function<void(ifstream&,ostringstream&)> test){
 	ifstream f(Utils::get_data_file_path(iff), ifstream::in);
 	ostringstream o;
-
-	int n, capacity, i;
-	f >> n >> capacity;
-	LRUCache l(capacity);
-	for (i = 0; i<n; i++) {
-		string command;
-		f >> command;
-		if (command == "get") {
-			int key;
-			f >> key;
-			o << l.get(key) << endl;
-		}
-		else if (command == "set") {
-			int key, value;
-			f >> key >> value;
-			l.set(key, value);
-		}
-	}
+	
+	test(f,o);
 
 	//read expected output
 	ifstream e(Utils::get_data_file_path(off), ifstream::in);
@@ -140,13 +131,127 @@ static std::pair<string,string> lru_cache_test(const char* iff, const char* off)
 	return make_pair(ss.str(), o.str());
 }
 
-TEST_CASE("LRU Cache: Test case 1", "[hackerrank]"){
-	auto r = lru_cache_test("hackerrank-lru-cache-1-input.txt", "hackerrank-lru-cache-1-output.txt");
+static pair<string,string> lru_cache_test(const char* iff, const char* off){
+	return hacker_ranker_tester(iff, off, [](ifstream& f, ostringstream& o) {
+    	int n, capacity, i;
+    	f >> n >> capacity;
+    	LRUCache l(capacity);
+    	for (i = 0; i<n; i++) {
+    		string command;
+    		f >> command;
+    		if (command == "get") {
+    			int key;
+    			f >> key;
+    			o << l.get(key) << endl;
+    		}
+    		else if (command == "set") {
+    			int key, value;
+    			f >> key >> value;
+    			l.set(key, value);
+    		}
+    	}
+	});
+}
+
+TEST_CASE("LRU Cache", "[hackerrank]"){
+	SECTION("Test case 1") {
+		auto r = lru_cache_test("hackerrank-lru-cache-1-input.txt", "hackerrank-lru-cache-1-output.txt");
+		REQUIRE(r.first == r.second);
+	}
+
+	SECTION("Test case 2") {
+		auto r = lru_cache_test("hackerrank-lru-cache-2-input.txt", "hackerrank-lru-cache-2-output.txt");
+		REQUIRE(r.first == r.second);
+	}
+}
+
+/* 
+Max value in contiguous subarray
+https://www.hackerrank.com/challenges/deque-stl
+
+Given a set of arrays of size and an integer , you have to find the maximum integer for each and every contiguous subarray of size for each of the given arrays.
+
+Input Format:
+First line of input will contain the number of test cases T. 
+For each test case, you will be given the size of array N and the size of subarray to be used K.
+This will be followed by the elements of the array Ai.
+
+Sample Input:
+2
+5 2
+3 4 6 3 4
+7 4
+3 4 5 8 1 4 10
+
+Sample Output:
+4 6 6 4
+8 8 8 10
+*/
+static void printKMax(int arr[], int n, int k,ostringstream& out) {
+	deque<int> q;
+	//slide the window along with the sub-array
+
+	//the first window, only value larger than the last seen value will survive and are sorted in descending order 
+	int i;
+	for (i = 0; i < k; ++i) {
+		while(!q.empty() && arr[q.back()] <= arr[i]) {
+			q.pop_back();
+		}
+		q.push_back(i);
+	}
+	// the head is the index of the max value
+	/* Examples:
+	   1) if the first sub-array is [4 1 2 3], then what's left in the queue is [4 3]
+	      4 will be evicted as window slides, 1,2 does not even enter the queue but that does not matter as 
+		    a) they are smaller than 3 and 
+			b) they will be evicted before 3 so they will never have chance
+
+			Note while loop is important otherwise in the above case 1 will survive in the queue while it is smaller than 3
+	   2) if the first sub-array is [1 2 3 4], then what's left in the queue is [4]
+	*/
+
+	// i is at the end of the first window now
+	for (; i < n; ++i) {
+		//the head of q is the max value of the previous window
+		out << arr[q.front()] << " ";
+
+		//evict those do not below to this window
+		while (!q.empty() && q.front() <= i - k) q.pop_front();
+
+		while (!q.empty() && arr[q.back()] <= arr[i]) {
+			q.pop_back();
+		}
+
+		q.push_back(i);
+	}
+	out << arr[q.front()] << endl;
+}
+
+static pair<string,string> contiguous_subarray_test(const char* iff, const char* off){
+	return hacker_ranker_tester(iff, off, [](ifstream& f, ostringstream& o) {
+		int t;
+		f >> t;
+		while (t > 0) {
+			int n, k;
+			f >> n >> k;
+			int i;
+			int* arr = new int[n];
+			for (i = 0; i < n; i++)
+				f >> arr[i];
+			printKMax(arr, n, k,o);
+			delete[]arr;
+			t--;
+		}
+	});
+}
+
+TEST_CASE("Max value in contiguous subarray : Test Case 0", "[hackerrank]") {
+	auto r = contiguous_subarray_test("hackerrank-contiguous-subarray-0-input.txt", "hackerrank-contiguous-subarray-0-output.txt");
 	REQUIRE(r.first == r.second);
 }
 
-TEST_CASE("LRU Cache: Test case 2", "[hackerrank]"){
-	auto r = lru_cache_test("hackerrank-lru-cache-2-input.txt", "hackerrank-lru-cache-2-output.txt");
+TEST_CASE("Max value in contiguous subarray : Test Case 1", "[hackerrank]") {
+	auto r = contiguous_subarray_test("hackerrank-contiguous-subarray-1-input.txt", "hackerrank-contiguous-subarray-1-output.txt");
 	REQUIRE(r.first == r.second);
 }
 
