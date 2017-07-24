504cf45a1aaaf2febe5a7e3c8a6ace1f4ca5d711
Mu Lu <murphytalk@gmail.com>
2017-07-24 21:20:44 +0900

    added 2 questions

--- src/algo/dp.cpp
+++ src/algo/dp.cpp
@@ -1,9 +1,10 @@
 #include "catch.hpp"
 #include <vector>
+#include "src/utils/utils.h"
 
 using namespace std;
 
-namespace DP{
+namespace Algo{
 
 static void fibonacci_recursive(size_t n, vector<int>& result) {
     //if (result.size() < n) result.resize(n);
@@ -56,6 +57,127 @@ TEST_CASE("fibonacci : long", "[dp]") {
         REQUIRE(r1 == r2);
     }
 }
+
+/* Find the number of consecutive numbers that can add up to the given summary.
+   e.g. for 15 , the answer is 3 because:
+   1) 15 = 7 + 8
+   2) 15 = 4 + 5 + 6
+   3) 15 = 1 + 2 + 3 + 4 + 5 
+
+   e.g. for 15, ther answer is 1 : 10 = 1 + 2 + 3 + 4
+*/
+//#define VERBOSE_find_consecutive_num_sum
+static int find_consecutive_num_sum(long long N)
+{
+	int count = 0;
+	long long start, end;
+	for (auto parts = 2; parts < N; ++parts) {
+		auto half = parts / 2;
+		auto middle = N / parts;
+		if (parts % 2 == 0) {
+			start = middle - (half - 1);
+			end = middle + half;
+		}
+		else {			
+			start = middle - half;
+			end = middle + half;
+		}
+		if (start <= 0) continue;
+		long long sum = 0;
+#ifdef VERBOSE_find_consecutive_num_sum
+		stringstream ss;
+#endif
+		for (long long i = start; i <= end; ++i) {
+			sum += i;
+#ifdef VERBOSE_find_consecutive_num_sum
+			ss << i <<" ";
+#endif
+			if (sum > N) break;
+		}
+		if (sum == N) {
+			++count;
+#ifdef VERBOSE_find_consecutive_num_sum
+			LOG <<"parts="<<parts<<":"<< ss.str() << endl;
+#endif
+		}
+	}
+	return count;
+}
+
+static int find_consecutive_num_sum_dp(long long N)
+{
+	int count = 0;
+	long long start = 1, end = 1;
+	long long sum = 1;
+
+	while (start <= N / 2){
+		if (sum < N){
+			++end;
+			sum += end;
+		}
+		else if (sum > N){
+			sum -= start;
+			++start;
+		}else if (sum == N){
+			++count;
+			sum -= start;
+			++start;
+		}
+	}
+	return count;
+}
+
+
+TEST_CASE("consecutive num sum : 15", "[algo]") {
+	REQUIRE(find_consecutive_num_sum(15) == 3);
+}
+
+TEST_CASE("consecutive num sum : 10", "[algo]") {
+	REQUIRE(find_consecutive_num_sum(10) == 1);
+}
+
+TEST_CASE("consecutive num sum : 100", "[algo]") {
+	REQUIRE(find_consecutive_num_sum(100) == 2);
+}
+
+TEST_CASE("consecutive num sum : 125", "[algo]") {
+	REQUIRE(find_consecutive_num_sum(125) == 3);
+}
+
+TEST_CASE("consecutive num sum dp : 15", "[dp]") {
+	REQUIRE(find_consecutive_num_sum_dp(15) == 3);
+}
+
+TEST_CASE("consecutive num sum dp : 10", "[dp]") {
+	REQUIRE(find_consecutive_num_sum_dp(10) == 1);
+}
+
+TEST_CASE("consecutive num sum dp : 100", "[dp]") {
+	REQUIRE(find_consecutive_num_sum_dp(100) == 2);
+}
+
+TEST_CASE("consecutive num sum dp : 125", "[dp]") {
+	REQUIRE(find_consecutive_num_sum_dp(125) == 3);
+}
+
+TEST_CASE("consecutive num sum dp : big number", "[dp][algo]") {
+	//https://en.wikipedia.org/wiki/Polite_number
+	//http://planetmath.org/politenumber
+	const long long n = 50000;
+	const long long N = n*(n + 1) / 2; //http://planetmath.org/node/31698
+	static int n1, n2;
+	SECTION("DP") {
+		n1 = find_consecutive_num_sum_dp(N);
+	}
+	SECTION("non-DP") {
+		n2 = find_consecutive_num_sum_dp(N);
+	}
+	SECTION("compare") {
+		REQUIRE(n1==n2);
+		LOG << "N="<<N<< ",DP solution:" << n1 << ",non-DP solution:" << n2 << endl;
+	}
+}
+
 
 
 }; //namespace DP
---
+++ src/algo/other.cpp
@@ -0,0 +1,55 @@
+#include "catch.hpp"
+#include <bitset>
+#include <vector>
+
+using namespace std;
+
+namespace Algo{
+
+/*
+Find number of duplicates.
+e.g. for input {1,1,1,2,3,4,5,5,6} the result should be 2 : 1 and 5 has duplicates
+The integer value range is -1000 ~ 1000
+*/
+static const int find_duplicates_min = -1000;
+static const int find_duplicates_max =  1000;
+
+static int find_duplicates(const vector<int>& nums) {
+	const int N = find_duplicates_max - find_duplicates_min + 1;
+	//one bitset to check if a given num has already been spot
+	bitset<N> seen;
+	//another bitset to check if a given num has already been confirmed to have duplicates, so we won't count it more than once
+	bitset<N> duplicates;
+
+	int count = 0;
+	for (int n : nums) {
+		int bit = n - find_duplicates_min; //to avoid negative bit index
+		if (!duplicates[bit]) {
+			if (seen[bit]) {
+				++count;
+				duplicates[bit] = true;
+			}
+			else seen[bit] = true;
+		}
+	}
+
+	return count;
+}
+
+TEST_CASE("find duplicates: 1,1,1,2,3,4,5,5,6,5,7", "[algo]") {
+	const vector<int> nums = { 1,1,1,2,3,4,5,5,6,5,7 };
+	REQUIRE(find_duplicates(nums) == 2);
+}
+
+TEST_CASE("find duplicates: from min to max with duplicates", "[algo]") {
+	vector<int> nums = { -1000,-1000,1000,1000 };
+	REQUIRE(find_duplicates(nums) == 2);
+}
+
+TEST_CASE("find duplicates: from min to max without duplicates", "[algo]") {
+	vector<int> nums = { -1000,0,100,200,1000 };
+	REQUIRE(find_duplicates(nums) == 0);
+}
+
+
+}
