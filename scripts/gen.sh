5908e622efc629cc5c548f1ddb6b53d286bc6d16
Mu Lu <murphytalk@gmail.com>
2019-03-31 11:56:07 +0900

    leave data/*.zip alone, they are for python tests and python tests will read from zip file directory

--- scripts/gen.sh
+++ scripts/gen.sh
@@ -6,9 +6,3 @@ ${SCRIPT_DIR}/gen_long_bit_string.py > ${DATA_DIR}/leetcode-add-binary.txt
 ${SCRIPT_DIR}/gen_long_string.py > ${DATA_DIR}/leetcode-longest-substr.txt
 ${SCRIPT_DIR}/gen_int_seq.py > ${DATA_DIR}/leetcode-int-seq.txt
 
-cd ${DATA_DIR} >/dev/null
-for f in *.zip;do
-	unzip -x $f
-done
-cd - >/dev/null
-
